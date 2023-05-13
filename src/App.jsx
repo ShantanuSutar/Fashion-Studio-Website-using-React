4d581895161e712a1927311e40966bc846b4c0ab
Shantanu Sutar <shantanusutar17@gmail.com>
2023-05-13 10:59:04 +0530

    adding locomotive scroll setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import { dark } from "./styles/Themes";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
 function App() {
   const containerRef = useRef(null);
 
@@ -9,7 +10,24 @@ function App() {
     <>
       <GlobalStyles />
       <ThemeProvider theme={dark}>
-        <main className="App">App File</main>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            Appp File
+          </main>
+        </LocomotiveScrollProvider>
       </ThemeProvider>
     </>
   );
