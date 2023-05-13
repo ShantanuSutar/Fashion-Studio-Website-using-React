bdf021a470fa068dbde7a2481b9e73b26474060c
Shantanu Sutar <shantanusutar17@gmail.com>
2023-05-13 10:53:31 +0530

    Adding global styles and themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import "./App.css";
-
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import { dark } from "./styles/Themes";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 function App() {
-  return <div className="App">Shantanu</div>;
+  const containerRef = useRef(null);
+
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={dark}>
+        <main className="App">App File</main>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/Themes.jsx
@@ -0,0 +1,20 @@
+export const dark = {
+  body: "#202020", //black shade
+  text: "fff",
+  textRgba: "32,32,32",
+  bodyRgba: "255,255,255",
+
+  grey: "#bebebe", //grey shade
+
+  //1em = 16px
+  fontxs: "0.75em",
+  fontsm: "0.875em",
+  fontmd: "1em",
+  fontlg: "1.25em",
+  fontxl: "2em",
+  fontxxl: "3em",
+  fontxxxl: "5em",
+  fontBig: "10em",
+
+  navHeight: "5rem",
+};
